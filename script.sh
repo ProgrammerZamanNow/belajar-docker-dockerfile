aae77a6062e8290c955dd3ed8965560cd08bf5fb
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-04-27 16:46:22 +0700

    belajar docker dockerfile

---
+++ script.sh
@@ -0,0 +1,21 @@
+
+# FROM Instruction
+docker build -t khannedy/from from
+
+docker image ls
+
+# RUN Instruction
+docker build -t khannedy/run run
+
+docker build -t khannedy/run run --progress=plain --no-cache
+
+# CMD Instruction
+docker build -t khannedy/command command
+
+docker image inspect khannedy/command
+
+docker container create --name command khannedy/command
+
+docker container start command
+
+docker container logs command
